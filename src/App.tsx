780d4fb4f153498ead09b97e1d54c1cc06a66a1a
kulinrangani <kulin.rangani@excellentwebworld.in>
2026-06-19 16:33:58 +0530

    feat: implement i18n localization support and configure language detection

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import "./utils/i18n";
 
 const queryClient = new QueryClient({
   defaultOptions: {
